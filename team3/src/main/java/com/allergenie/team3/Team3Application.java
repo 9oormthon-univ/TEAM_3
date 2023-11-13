0431b8a5e13ec7aca5db6979e027cd0f877e32d8
CYJhub <yejee1018@naver.com>
2023-11-13 16:32:06 +0900

    initial setting

---
+++ team3/src/main/java/com/allergenie/team3/Team3Application.java
@@ -0,0 +1,13 @@
+package com.allergenie.team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team3Application.class, args);
+	}
+
+}
---
+++ team3/src/test/java/com/allergenie/team3/Team3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allergenie.team3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
