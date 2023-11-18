e9a7c277d8c36814efb2d69272aa9f169a4487a1
yoonsseo <yellisa2000@gmail.com>
2023-11-18 17:40:01 +0900

    [Feat] Dto 수정 - 사용자 이미지 추가 #12

--- src/main/java/com/allergenie/server/controller/MedicineController.java
+++ src/main/java/com/allergenie/server/controller/MedicineController.java
@@ -1,5 +1,7 @@
 package com.allergenie.server.controller;
 
+import com.allergenie.server.config.jwt.JwtTokenProvider;
+import com.allergenie.server.domain.User;
 import com.allergenie.server.dto.response.MedicineInfoDto;
 import com.allergenie.server.dto.response.MedicineListDto;
 import com.allergenie.server.service.MedicineService;
@@ -8,22 +10,28 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/home")
 public class MedicineController {
     private final MedicineService medicineService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @GetMapping
     public MedicineListDto getMedicineListBySearch(
             @RequestParam(name = "search") String search,
-            @RequestParam(name = "pageNo") int pageNo) {
+            @RequestParam(name = "pageNo") int pageNo,
+            HttpServletRequest request) {
+        User user = jwtTokenProvider.getUserInfoByToken(request);
         Pageable pageable = PageRequest.of(pageNo, 3);
-        return medicineService.getMedicineListBySearch(search, pageable);
+        return medicineService.getMedicineListBySearch(user, search, pageable);
     }
 
     @GetMapping("/{medicineId}")
-    public MedicineInfoDto getMedecineInfo(@PathVariable Long medicineId) {
-        return medicineService.getMedicineInfo(medicineId);
+    public MedicineInfoDto getMedecineInfo(@PathVariable Long medicineId, HttpServletRequest request) {
+        User user = jwtTokenProvider.getUserInfoByToken(request);
+        return medicineService.getMedicineInfo(user, medicineId);
     }
 }
--- src/main/java/com/allergenie/server/dto/response/MedicineListDto.java
+++ src/main/java/com/allergenie/server/dto/response/MedicineListDto.java
@@ -10,12 +10,14 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class MedicineListDto {
+    private String userImageURL;
     private int totalPage;
     private int currentPage;
     private List<MedicineDto> medicineList;
 
     @Builder
-    public MedicineListDto(int totalPage, int currentPage, List<MedicineDto> medicineList) {
+    public MedicineListDto(int totalPage, int currentPage, String userImageURL, List<MedicineDto> medicineList) {
+        this.userImageURL = userImageURL;
         this.totalPage = totalPage;
         this.currentPage = currentPage;
         this.medicineList = medicineList;
