db39526cbd51a33f6bafdb21d7b7795600f67ec7
yoonsseo <yellisa2000@gmail.com>
2023-11-18 20:23:01 +0900

    [Feat] 약 정보 가져오는 거 수정 #9

--- src/main/java/com/allergenie/server/controller/DataController.java
+++ src/main/java/com/allergenie/server/controller/DataController.java
@@ -4,6 +4,7 @@ import com.allergenie.server.service.DataService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -15,7 +16,7 @@ public class DataController {
     private final DataService dataService;
 
     @GetMapping("/list")
-    public String getDataList() throws IOException {
-        return dataService.getDataList();
+    public String getDataList(@RequestParam(name = "pageNo") String pageNo) throws IOException {
+        return dataService.getDataList(pageNo);
     }
 }
